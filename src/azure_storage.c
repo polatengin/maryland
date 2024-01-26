17d97baecdbb82fef1059ea19f2f013bc5f51d5a
polatengin <enpolat@microsoft.com>
2024-01-26 04:44:22 +0000

    adding init handler

--- src/azure_storage.c
+++ src/azure_storage.c
@@ -5,3 +5,10 @@ MODULE_LICENSE("MIT");
 MODULE_AUTHOR("Engin Polat");
 MODULE_DESCRIPTION("A simple kernel module to map a local directory to Azure Blob Storage");
 
+static int __init module_init(void) {
+  pr_info("Azure Storage Module loaded to memory\n");
+  return 0;
+}
+
+
+module_init(module_init);
