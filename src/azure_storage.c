4170aca576416bcc66c684f6c897a218950e6810
polatengin <enpolat@microsoft.com>
2024-01-26 04:43:44 +0000

    creating initial module

---
+++ src/azure_storage.c
@@ -0,0 +1,7 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("MIT");
+MODULE_AUTHOR("Engin Polat");
+MODULE_DESCRIPTION("A simple kernel module to map a local directory to Azure Blob Storage");
+
